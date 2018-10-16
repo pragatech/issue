ecac6fe4115fdd6611d440583e73a661e5bb1196
pragatech <pragatheswaran.a.c@hotmail.com>
2018-10-16 22:53:03 +0530

    2nd step

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -1,14 +1,38 @@
 defmodule Issues.CLI do
   @default_count 4
 
+  @moduledoc """
+  Handle command line parsing and dispatch
+  to the various functions that end up generating
+  tables for n_of_issues in a git hub project
+  """
   def run argv do
-    parse_args argv
+    argv
+    |> parse_args
+    |> process
   end
 
+  @doc """
+  argv can have -h or --help, which return help
+
+  otherwise, it will have github user name, project and no of issues count(optional)
+  it returns, `{user, project,count}`
+  """
   def parse_args(argv) do
     OptionParser.parse(argv, switches: [help: :boolean], aliases: [h: :help])
     |> elem(1)
     |> args_to_internal_represntation
+  end
+
+  def process(:help) do
+    IO.puts """
+      usage: issues <user> <project> [count| #{@default_count}]
+    """
+    System.halt(0)
+  end
+
+  def process({user,project,_count}) do
+    Issues.GithubIssues.fetch(user,project)
   end
 
   defp args_to_internal_represntation([user, project, count]) do
